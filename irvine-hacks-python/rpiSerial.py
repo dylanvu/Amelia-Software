5031e0f3284b56e2c9f89d4b2e786e18d16d6041
Dylan Vu <dylanvu9@gmail.com>
2024-01-28 03:52:33 -0800

    convert to bytes

--- irvine-hacks-python/rpiSerial.py
+++ irvine-hacks-python/rpiSerial.py
@@ -3,4 +3,6 @@ import serial
 def sendCommand(command):
     ser = serial.Serial("/dev/ttyUSB0", 9600)
     print(f"Sending {command}")
-    ser.write(command)
+    # convert the string to bytes
+    commandBytes = command.encode('utf-8')
+    ser.write(commandBytes)
