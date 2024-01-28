8328573bed654f8e924d1e057004edaf3fca7e54
Dylan Vu <dylanvu9@gmail.com>
2024-01-28 03:50:20 -0800

    change to be USB0 for arduino

--- irvine-hacks-python/rpiSerial.py
+++ irvine-hacks-python/rpiSerial.py
@@ -1,6 +1,6 @@
 import serial
 
 def sendCommand(command):
-    ser = serial.Serial("/dev/ttyS0", 9600)
+    ser = serial.Serial("/dev/ttyUSB0", 9600)
     print(f"Sending {command}")
     ser.write(command)
